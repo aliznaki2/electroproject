b84738022c6fef2b5ee307c578d7a20dd75d3e20
aliznaki <aliznaki4@gmail.com>
2025-01-24 12:37:14 +0100

    Full Project Commit

---
+++ js/index.js
@@ -0,0 +1,70 @@
+var i=1
+function pc(){
+    document.getElementById('pc').setAttribute("src",`image/OrdinateurPortable/Ultrabook/pc`+i+'.jpg')
+    ++i
+    if(i==5) i=1 ;
+}
+setInterval(pc,3000)
+var j=1
+function pwpnt(){
+    document.getElementById('pwpnt').setAttribute("src",`image/OrdinateurPortable/Ultrabook/pwpnt`+j+'.jpg')
+    ++j
+    if(j==4) j=1 ;
+}
+setInterval(pwpnt,3000)
+var k=1
+function pcgmr(){
+    document.getElementById('pcgmr').setAttribute("src",`image/OrdinateurPortable/Ultrabook/pcgmr`+k+'.jpg')
+    ++k
+    if(k==4) k=1 ;
+}
+setInterval(pcgmr,3000)
+var d=1
+function Ultrabookecron(){
+    document.getElementById('Ultrabook-ecron').setAttribute("src",`image/OrdinateurPortable/Ultrabook/ecro`+d+'.jpg')
+    ++d
+    if(d==3) d=1 ;
+}
+setInterval(pcgmr,3000)
+var d=1
+function clavierCas(){
+    document.getElementById('clavierCas').setAttribute("src",`image/OrdinateurPortable/Ultrabook/ecro`+d+'.jpg')
+    ++d
+    if(d==3) d=1 ;
+}
+setInterval(Ultrabookecron,3000)
+var h=1
+function imprimante(){
+    document.getElementById('imprimante').setAttribute("src",`image/OrdinateurPortable/Ultrabook/impremante`+h+'.jpg')
+    ++h
+    if(h==3) h=1 ;
+}
+setInterval(imprimante,3000)
+var s=1
+function ipad(){
+    document.getElementById('ipad').setAttribute("src",`image/OrdinateurPortable/Ultrabook/ipad`+s+'.jpg')
+    ++s
+    if(s==5) s=1 ;
+}
+setInterval(ipad,3000)
+
+var a=1
+function phone(){
+    document.getElementById('phone').setAttribute("src",`image/OrdinateurPortable/Ultrabook/phone`+a+'.jpg')
+    ++a
+    if(a==4) a=1 ;
+}
+setInterval(phone,3000)
+var x=1
+function airpods(){
+    document.getElementById('airpods').setAttribute("src",`image/OrdinateurPortable/Ultrabook/airpods`+x+'.jpg')
+    ++x
+    if(x==4) x=1 ;
+}
+setInterval(airpods,3000)
+function clavierCas(){
+    document.getElementById('clavierCas').setAttribute("src",`image/OrdinateurPortable/Ultrabook/clavierCas`+x+'.jpg')
+    ++x
+    if(x==4) x=1 ;
+}
+setInterval(clavierCas,3000)
